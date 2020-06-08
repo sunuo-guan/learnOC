eb694ff2358161a5974119b9255437a7e5290297
guanzhihao <guanzhihao@kuaishou.com>
2020-06-08 15:52:05 +0800

    create the partOne

---
+++ PartOne/PartOne/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  PartOne
+//
+//  Created by 关智豪 on 2020/6/8.
+//  Copyright © 2020 关智豪. All rights reserved.
+//
+
+
+//单行注释
+/*
+ 多行注释
+ */
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int sum=50+20;
+        NSLog(@"Hello, World!\nHello, gif");
+        NSLog(@"Hello, OC!");
+        NSLog(@"50 plus 20 and %i", sum);
+    }
+    return 0;
+}
