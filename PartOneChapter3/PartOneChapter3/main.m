eb03db713c7dec5e9c966acdde2479c1ca5a16fa
guanzhihao <guanzhihao@kuaishou.com>
2020-06-08 16:48:17 +0800

    add class Fraction

--- PartOneChapter3/PartOneChapter3/main.m
+++ PartOneChapter3/PartOneChapter3/main.m
@@ -8,10 +8,63 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Fraction : NSObject
+
+- (void) print;
+- (void) setNumberator: (int) n;
+- (void) setDenominator: (int) d;
+- (int) getNumberator;
+- (int) getDenominator;
+
+@end
+
+@implementation Fraction
+{
+    int numberator;
+    int denominator;
+}
+
+- (void) print
+{
+    NSLog(@"%i/%i", numberator, denominator);
+}
+
+- (void) setNumberator: (int) n
+{
+    numberator = n;
+}
+
+- (void) setDenominator: (int) d
+{
+    denominator = d;
+}
+
+- (int) getNumberator
+{
+    return numberator;
+}
+
+- (int) getDenominator
+{
+    return denominator;
+}
+
+@end
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //Fraction *myFraction;
+        
+        //myFraction = [Fraction alloc];
+        //myFraction = [myFraction init];
+        Fraction *myFraction = [[Fraction alloc] init];
+        
+        [myFraction setNumberator: 1];
+        [myFraction setDenominator: 3];
+        
+        NSLog(@"The value of fraction is: %i/%i", [myFraction getNumberator], [myFraction getDenominator]);
+        //[myFraction print];
     }
     return 0;
 }
