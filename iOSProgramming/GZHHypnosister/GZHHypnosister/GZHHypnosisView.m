cff18ad69c68a4f9b0b82a061dd92f976e7a3edb
guanzhihao <guanzhihao@kuaishou.com>
2020-06-12 17:07:35 +0800

    draw many hypnosister

--- iOSProgramming/GZHHypnosister/GZHHypnosister/GZHHypnosisView.m
+++ iOSProgramming/GZHHypnosister/GZHHypnosister/GZHHypnosisView.m
@@ -39,16 +39,32 @@
     center.y = bounds.origin.y + bounds.size.height/2.0;
     
     //取最小值计算半径
-    float radius = (MIN(bounds.size.height, bounds.size.width) / 2.0);
+    //float radius = (MIN(bounds.size.height, bounds.size.width) / 2.0);
+    
+    //最外层圆作为视图外接圆
+    float maxRadius = hypotf(bounds.size.height, bounds.size.width) / 2.0;
     
     //创建绘图UIBezierPath类的对象绘制圆
     UIBezierPath *path = [[UIBezierPath alloc]init];
     //定义路径
+    /*
     [path addArcWithCenter:center
                     radius:radius
                 startAngle:0.0
                   endAngle:2 * M_PI
                  clockwise:YES];
+     */
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -=20) {
+        //抬起画笔先移到起点
+        [path moveToPoint:CGPointMake(center.x+currentRadius, center.y)];
+        [path addArcWithCenter:center
+            radius:currentRadius
+        startAngle:0.0
+          endAngle:2 * M_PI
+         clockwise:YES];
+    }
+    
+    
     //设置线条宽度
     path.lineWidth = 10;
     //设置绘制线条的颜色
@@ -56,7 +72,10 @@
     //开始绘制
     [path stroke];
     
-    
+    //添加图片
+    UIImage *image = [UIImage imageNamed:@"0.jpeg"];
+    CGRect imageRect = CGRectMake(100, 100, 200, 200);
+    [image drawInRect:imageRect];
 }
 
 @end
