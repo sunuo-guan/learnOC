fc76a71a558639a5225c1773890c62c7257afecb
guanzhihao <guanzhihao@kuaishou.com>
2020-06-15 20:25:39 +0800

    create textField

--- iOSProgramming/Hypno/Hypno/GZHHypnosisViewController.m
+++ iOSProgramming/Hypno/Hypno/GZHHypnosisViewController.m
@@ -9,14 +9,11 @@
 #import "GZHHypnosisViewController.h"
 #import "GZHHypnosisView.h"
 
-@implementation GZHHypnosisViewController
+@interface GZHHypnosisViewController() <UITextFieldDelegate>
 
-- (void)loadView
-{
-    //将新创建的对象赋予view属性
-    GZHHypnosisView *backgroundView = [[GZHHypnosisView alloc]init];
-    self.view = backgroundView;
-}
+@end
+
+@implementation GZHHypnosisViewController
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,6 +26,61 @@
         self.tabBarItem.image = image;
     }
     return self;
+}
+
+- (void)loadView
+{
+    //将新创建的对象赋予view属性
+    //GZHHypnosisView *backgroundView = [[GZHHypnosisView alloc]init];
+    //self.view = backgroundView;
+    CGRect frame = [UIScreen mainScreen].bounds;
+    GZHHypnosisView *backgroundView = [[GZHHypnosisView alloc]initWithFrame:frame];
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc]initWithFrame:textFieldRect];
+    //设置边框样式
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    
+    //修改UITextField属性
+    textField.placeholder = @"Hypnotize me";   //默认的占位文本
+    textField.returnKeyType = UIReturnKeyDone;   //换行键显示为done
+    
+    //设置委托为控制器自身
+    textField.delegate = self;
+    
+    [backgroundView addSubview:textField];
+    self.view = backgroundView;
+}
+
+- (void)drawHypnoticMessage:(NSString *)message
+{
+    for (int i = 0; i < 20; i++) {
+        UILabel *messageLabel = [[UILabel alloc]init];
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor blackColor];
+        messageLabel.text = message;
+        //根据文字调整label大小
+        [messageLabel sizeToFit];
+        //x的坐标
+        int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int x = arc4random() % width;
+        //y的坐标
+        int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int y = arc4random() % height;
+        //label的frame
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        //
+        [self.view addSubview:messageLabel];
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
