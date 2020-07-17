70283eac0e9b58019c0d0d6bdeab5890d4b61016
guanzhihao <guanzhihao@kuaishou.com>
2020-07-17 16:37:09 +0800

    optimization

--- sampleApp/sampleApp/AppDelegate.m
+++ sampleApp/sampleApp/AppDelegate.m
@@ -17,28 +17,27 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    
+
     UITabBarController *tbc = [[UITabBarController alloc] init];
     UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:tbc];
-    
-    GZHNewsViewController *vc0 = [[GZHNewsViewController alloc] init];
-    GZHVideoViewController *vc1 = [[GZHVideoViewController alloc] init];
-    GZHRecommendViewController *vc2 = [[GZHRecommendViewController alloc] init];
-    
+
+    GZHNewsViewController *newsViewController = [[GZHNewsViewController alloc] init];
+    GZHVideoViewController *videoViewController = [[GZHVideoViewController alloc] init];
+    GZHRecommendViewController *recommendViewController = [[GZHRecommendViewController alloc] init];
+
     UIViewController *vc3 = [[UIViewController alloc] init];
     vc3.view.backgroundColor = [UIColor whiteColor];
     vc3.tabBarItem.title = @"我的";
-    
-    [tbc setViewControllers:@[vc0,vc1,vc2,vc3]];
+
+    [tbc setViewControllers:@[newsViewController, videoViewController, recommendViewController, vc3]];
     tbc.delegate = self;
-    
+
     [self.window setRootViewController:nc];
     [self.window makeKeyAndVisible];
-    
+
     return YES;
 }
 
